e1070cb95f7bc5c77fa02f6c556cfd540168da56
kdiraydn <65663370+kdiraydn@users.noreply.github.com>
2022-10-22 22:20:20 +0300

    SimpsonBook

---
+++ SimpsonBook/SimpsonBook/Simpson.swift
@@ -0,0 +1,22 @@
+//
+//  Simpson.swift
+//  SimpsonBook
+//
+//  Created by Abdulkadir Aydın on 11.10.2022.
+//
+
+import Foundation
+import UIKit
+
+class Simpson{
+    
+    var name : String
+    var job : String
+    var image : UIImage
+    
+    init(simpsonName: String, simpsonJob: String, simpsonImage: UIImage) {
+        self.name = simpsonName
+        self.job = simpsonJob
+        self.image = simpsonImage
+    }
+}
