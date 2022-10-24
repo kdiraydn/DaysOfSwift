786e98bd26fe275504ddf7a78f9779594879794e
kdiraydn <65663370+kdiraydn@users.noreply.github.com>
2022-10-24 19:18:06 +0300

    TimerProject

---
+++ TimerProject/TimerProject/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  TimerProject
+//
+//  Created by Abdulkadir Aydın on 27.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    var timer = Timer()
+    var counter = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        counter = 10
+        timeLabel.text = "Time: \(counter)"
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerFunction), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func timerFunction() {
+        timeLabel.text = "Time: \(counter)"
+        counter -= 1
+        
+        if counter == 0 {
+            timer.invalidate()
+            timeLabel.text = "time is over"
+        }
+    }
+    
+
+
+
+    @IBAction func buttonClicked(_ sender: Any) {
+        print("button clicked")
+    }
+}
+
